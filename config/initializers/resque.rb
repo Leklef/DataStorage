3ea0c753d41d7bc1014d373cf95c55d8702765cc
Lenar Gilyazov <lgilyazovg@gmail.com>
2018-11-03 13:00:55 +0300

    исправил resque url

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,7 +3,7 @@ require 'resque-scheduler'
 if ENV["REDISCLOUD_URL"]
   redis_url = ENV["REDISCLOUD_URL"]
 end
-uri = URI.parse($redis.nil? ? "redis://localhost:6379/" : redis_url)
+uri = URI.parse(redis_url.nil? ? "redis://localhost:6379/" : redis_url)
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
