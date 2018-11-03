2f52f75943ea4319fc52b6530f986bae622bc38d
Lenar Gilyazov <lgilyazovg@gmail.com>
2018-11-03 14:00:07 +0300

    исправил resque url

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,4 +4,4 @@ uri = URI.parse(ENV["REDISCLOUD_URL"] || "redis://localhost:6379/")
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
-Resque.enqueue_at(1.minutes.from_now, DataStorageJob)
+Resque.enqueue_at(20.hours.from_now, DataStorageJob)
